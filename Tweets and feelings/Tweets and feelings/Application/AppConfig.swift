53d624f80cef30fc57dd21d76307fd4e8a4f4fa8
Nadeesha Lakmal <>
2019-05-19 16:47:28 +0530

    Initially adding project into GitGub

---
+++ Tweets and feelings/Tweets and feelings/Application/AppConfig.swift
@@ -0,0 +1,227 @@
+//
+//  AppConfig.swift
+//  Tweets and feelings
+//
+//  Created by Nadeesha Lakmal on 5/19/19.
+//  Copyright © 2019 Nadeesha Lakmal. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+let configFileName:String = "Configuration"
+
+class AppConfig: NSObject, CLLocationManagerDelegate {
+	
+	static let si               = AppConfig()
+	private var configDict      : NSDictionary?
+	
+	let defaults            	= UserDefaults.standard
+	
+	var appName: String! {
+		guard let string        = getValue(key: "appName") else { return "" }
+		return (string as! String)
+	}
+	
+	// MARK: - url variables
+	var url: NSDictionary! {
+		guard let urlArray = getValue(key: "url") else { return [:] }
+		return (urlArray as! NSDictionary)
+	}
+	
+	var baseUrl: String! {
+		guard let string = url["domain"] else { return "" }
+		return (string as! String)
+	}
+	
+	var baseUrlTwitter: String! {
+		guard let string = url["domainTwitter"] else { return "" }
+		return (string as! String)
+	}
+	
+	var baseUrlGoogleDocAnalyse: String! {
+		guard let string = url["domainGoogleDocAnalyse"] else { return "" }
+		return (string as! String)
+	}
+	
+	
+	// MARK: - API keys
+	var apiKeys: NSDictionary! {
+		guard let keysArray = getValue(key: "apiKeys") else { return [:] }
+		return (keysArray as! NSDictionary)
+	}
+	
+	var twitterAPIKey: String! {
+		guard let string = apiKeys["twitterAPIKey"] else { return "" }
+		return (string as! String)
+	}
+	
+	var twitterAPISecretKey: String! {
+		guard let string = apiKeys["twitterAPISecretKey"] else { return "" }
+		return (string as! String)
+	}
+	
+	var twitterBearer: String! {
+		guard let string = apiKeys["twitterBearer"] else { return "" }
+		return (string as! String)
+	}
+	
+	var googleAPIKey: String! {
+		guard let string = apiKeys["googleAPIKey"] else { return "" }
+		return (string as! String)
+	}
+	
+
+	
+	// MARK: - language variables
+	var language                = ""
+	
+	var defaultLang: String! {
+		guard let lang = getValue(key: "defaultLang") else { return "" }
+		return (lang as! String)
+	}
+	
+	internal func setLng( newLanguage: NSString) {
+		self.language           = newLanguage as String
+		defaults.set(language, forKey: "language")
+	}
+	
+	var languages: NSDictionary! {
+		guard let langs = getValue(key: "languages") else { return [:] }
+		return (langs as! NSDictionary)
+	}
+	
+	var languageStrings: NSDictionary! {
+		guard let langStrings = languages[language] else { return [:] }
+		return (langStrings as! NSDictionary)
+	}
+	
+	var networkFailedAlertTitle:String! {
+		guard let string = languageStrings["networkFailedAlertTitle"] else { return "" }
+		return (string as! String)
+	}
+	
+	var networkFailedAlertMsg:String! {
+		guard let string = languageStrings["networkFailedAlertMsg"] else { return "" }
+		return (string as! String)
+	}
+	
+	
+	var somethingWentWrongAlertMsg:String! {
+		guard let string = languageStrings["somethingWentWrongAlertMsg"] else { return "" }
+		return (string as! String)
+	}
+	
+	
+	var tryAgainAlertMsg:String! {
+		guard let string = languageStrings["tryAgainAlertMsg"] else { return "" }
+		return (string as! String)
+	}
+	
+	var pleaseTryAgainLaterAlertMsg:String! {
+		guard let string = languageStrings["pleaseTryAgainLaterAlertMsg"] else { return "" }
+		return (string as! String)
+	}
+	
+	var pleaseFillTheUsernameAlertMsg:String! {
+		guard let string = languageStrings["pleaseFillTheUsernameAlertMsg"] else { return "" }
+		return (string as! String)
+	}
+	
+	// MARK: - system variables
+	var memoryCapacity:Int {
+		guard let int = getValue(key: "memoryCapacity") else { return 0 }
+		return (int as! Int) * 1024 * 1024
+	}
+	
+	var diskCapacity:Int {
+		guard let int = getValue(key: "diskCapacity") else { return 0 }
+		return (int as! Int) * 1024 * 1024
+	}
+	
+	var urlCache:URLCache {
+		let cache = URLCache(memoryCapacity: memoryCapacity, diskCapacity: diskCapacity, diskPath: "TweetsAndFeelingsDiskpath")
+		return cache
+	}
+	
+	let screenSize                  : CGRect    = UIScreen.main.bounds
+	
+	//MARK: - Colors
+	let colorPrimary                            = UIColor(hexString: "2878B2")
+	
+	//MARK: - RestClientError constants
+	var restClientErrors: NSDictionary! {
+		guard let dict = getValue(key: "restClientErrors") else { return [:] }
+		return (dict as! NSDictionary)
+	}
+	
+	var localErrors: NSDictionary! {
+		guard let dict = restClientErrors["localErrors"] else { return [:] }
+		return (dict as! NSDictionary)
+	}
+	
+	var serverErrors: NSDictionary! {
+		guard let dict = restClientErrors["serverErrors"] else { return [:] }
+		return (dict as! NSDictionary)
+	}
+	
+	var jsonParseError: (code: Int, msg: String) {
+		let dict    = localErrors["jsonParseError"] as! NSDictionary
+		return (dict["code"] as! Int ,msg: dict["message"] as! String)
+	}
+	
+	var undefinedError: (code: Int, msg: String) {
+		let dict    = serverErrors["undefinedError"] as! NSDictionary
+		return (dict["code"] as! Int ,msg: dict["message"] as! String)
+	}
+	
+	
+	//MAEK: - Specific image names
+	let default_ImageName           = "default_image"
+	
+	//MARK: - init
+	override private init() {
+		super.init()
+		self.startCofigManager()
+	}
+	
+	func startCofigManager() {
+		if let plist = Plist(name: configFileName) {
+			configDict = plist.getValuesInPlistFile()
+		} else {
+			print("AppConfig -> startManager : Unable to start")
+		}
+	}
+	
+	func getValue(key:String) -> AnyObject? {
+		var value:AnyObject?
+		if let dict = configDict {
+			let keys = Array(dict.allKeys)
+			//print("[Config] Keys are: \(keys)")
+			if keys.count != 0 {
+				for (_,element) in keys.enumerated() {
+					//print("[Config] Key Index - \(index) = \(element)")
+					if element as! String == key {
+						//print("[Config] Found the Item that we were looking for for key: [\(key)]")
+						value = dict[key]! as AnyObject
+						break
+					}
+				}
+				
+				if value != nil {
+					return value!
+				} else {
+					print("[AppConfig] WARNING: The Item for key '\(key)' does not exist! Please, check your spelling.")
+					return .none
+				}
+			} else {
+				print("[AppConfig] No Plist Item Found when searching for item with key: \(key). The Plist is Empty!")
+				return .none
+			}
+		} else {
+			print("[AppConfig] -> getValue : Unable to get Plist")
+			return .none
+		}
+	}
+	
+}
---
+++ Tweets and feelings/Tweets and feelings/Utilities/Extensions.swift
@@ -0,0 +1,273 @@
+//
+//  Extensions.swift
+//  Extensions
+//
+//  Created by Nadeesha Lakmal on 11/1/17.
+//  Copyright © 2017 NadeeshaC32. All rights reserved.
+//
+
+import UIKit
+
+public enum GradientDirection {
+    case leftToRight
+    case rightToLeft
+    case topToBottom
+    case bottomToTop
+}
+
+extension UIView {
+    func addShadow() {
+        self.layer.shadowColor                      = UIColor.black.cgColor
+        self.layer.shadowOffset                     = CGSize(width: 0, height: 2)
+        self.layer.shadowOpacity                    = 0.2
+        self.layer.shadowRadius                     = 1.0
+    }
+    
+    func setConstraintsFor(contentView: UIView, left: Bool = true, top: Bool = true, right: Bool = true, bottom: Bool = true) {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(contentView)
+        
+        var constraints         : [NSLayoutConstraint] = []
+        if left {
+            let constraintLeft      = NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0)
+            constraints.append(constraintLeft)
+        }
+        
+        if top {
+            let constraintTop       = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0)
+            constraints.append(constraintTop)
+        }
+        
+        if right {
+            let constraintRight     = NSLayoutConstraint(item: contentView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0)
+            constraints.append(constraintRight)
+        }
+        
+        if bottom {
+            let constraintBottom    = NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
+            constraints.append(constraintBottom)
+        }
+        
+        self.addConstraints(constraints)
+    }
+    
+    func addGradientWithColors(color1: UIColor, color2: UIColor, direction: GradientDirection) {
+        let gradient            = CAGradientLayer()
+        gradient.frame          = self.bounds
+        
+        switch direction {
+        case .leftToRight:
+            gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        case .rightToLeft:
+            gradient.startPoint = CGPoint(x: 1.0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 0.0, y: 0.5)
+        case .bottomToTop:
+            gradient.startPoint = CGPoint(x: 0.5, y: 1.0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 0.0)
+        case .topToBottom:
+            gradient.startPoint     = CGPoint(x: 0.5, y: 0.0)
+            gradient.endPoint       = CGPoint(x: 0.5, y: 1.0)
+        }
+        
+        gradient.colors         = [color1.cgColor, color2.cgColor]
+        
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+    
+    func addBoarder(width: CGFloat, cornerRadius: CGFloat, color: UIColor) {
+        self.layer.cornerRadius     = cornerRadius
+        self.layer.borderColor      = color.cgColor
+        self.layer.borderWidth      = width
+        self.clipsToBounds          = true
+    }
+    
+    func removeAllSubViews() {
+        for view in self.subviews {
+            view.removeFromSuperview()
+        }
+    }
+    
+    func addAsPopupView() {
+        self.alpha                  = 0
+        UIApplication.shared.keyWindow?.setConstraintsFor(contentView: self)
+        UIView.animate(withDuration: 0.25) {
+            self.alpha              = 1
+        }
+    }
+    
+    func removePopupView(completion: (() -> Void)? = nil) {
+        UIView.animate(withDuration: 0.25, animations: { 
+            self.alpha              = 0
+        }) { (completed) in
+            self.removeFromSuperview()
+            completion?()
+        }
+    }
+}
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: CGFloat(9999))
+		let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return boundingBox.height
+    }
+    
+    func height(constraintedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let label =  UILabel(frame: CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        label.text = self
+        //        label.sizeToFit()
+        
+        let neededSize = label.sizeThatFits(CGSize(width: width, height: CGFloat.greatestFiniteMagnitude))
+        //        print("neededSize.height:   \(neededSize.height)")
+        return neededSize.height
+    }
+    
+    func chopPrefix(_ count: Int = 1) -> String {
+	
+		let reqIndex = index(startIndex, offsetBy: 3)
+		return String(self[..<reqIndex])
+//        return substring(from: index(startIndex, offsetBy: count))
+    }
+    
+    func chopSuffix(_ count: Int = 1) -> String {
+		let reqIndex = index(endIndex, offsetBy: -count)
+		return String(self[reqIndex...])
+//        return substring(to: index(endIndex, offsetBy: -count))
+    }
+}
+
+
+extension Date {
+    func isBetweeen(date date1: Date, andDate date2: Date) -> Bool {
+        return date1.compare(self).rawValue * self.compare(date2).rawValue >= 0
+    }
+    
+    func isBetweeenExclusiveRange(date date1: Date, andDate date2: Date) -> Bool {
+        return date1.compare(self) == self.compare(date2)
+    }
+    
+    var millisecondsSince1970:Int {
+        return Int((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+    
+    init(milliseconds:Int) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds / 1000))
+    }
+}
+
+
+extension UIImageView {
+    func setImageWith(imagePath: String) {
+        var image                           = UIImage()
+        if let url = URL(string: imagePath), imagePath.range(of:"http") != nil {
+            //              print("imageName url:   \(url)")
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                if error != nil {
+                    print("load user image error : \(String(describing: error))")
+                }
+                
+                if let data = data {
+                    let imageFromData           = UIImage(data: data)
+                    DispatchQueue.main.async(execute: {
+                        if imagePath != "" && imageFromData != nil {
+                            image               = imageFromData!
+                        }
+                        self.image              = image
+                    })
+                }
+            }).resume()
+        } else {
+            print("imageName : error")
+        }
+    }
+}
+
+
+extension Int {
+    func format(f: String) -> String {
+//        let someInt = 4, someIntFormat = "03"
+//        println("The integer number \(someInt) formatted with \"\(someIntFormat)\" looks like \(someInt.format(someIntFormat))")
+//        // The integer number 4 formatted with "03" looks like 004
+        return String(format: "%\(f)d", self)
+    }
+}
+
+extension Double {
+    func format(f: String) -> String {
+//        let someDouble = 3.14159265359, someDoubleFormat = ".3"
+//        println("The floating point number \(someDouble) formatted with \"\(someDoubleFormat)\" looks like \(someDouble.format(someDoubleFormat))")
+//        // The floating point number 3.14159265359 formatted with ".3" looks like 3.142
+        return String(format: "%\(f)f", self)
+    }
+}
+
+extension String {
+    var hex: Int? {
+        return Int(self, radix: 16)
+    }
+}
+
+extension UIColor {
+    convenience init(hex: Int) {
+        self.init(hex: hex, a: 1.0)
+    }
+    
+    convenience init(hex: Int, a: CGFloat) {
+        self.init(r: (hex >> 16) & 0xff, g: (hex >> 8) & 0xff, b: hex & 0xff, a: a)
+    }
+    
+    convenience init(r: Int, g: Int, b: Int) {
+        self.init(r: r, g: g, b: b, a: 1.0)
+    }
+    
+    convenience init(r: Int, g: Int, b: Int, a: CGFloat) {
+        self.init(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: a)
+    }
+    
+    convenience init?(hexString: String) {
+        guard let hex = hexString.hex else {
+            return nil
+        }
+        self.init(hex: hex)
+    }
+}
+
+extension UINavigationController {
+    
+    func backToViewController(viewController: Swift.AnyClass) {
+        
+        for element in viewControllers as Array {
+            if element.isKind(of: viewController) {
+                self.popToViewController(element, animated: true)
+                break
+            }
+        }
+    }
+}
+
+
+extension UITableViewCell {
+    
+    func showSeparator() {
+		DispatchQueue.main.async { [weak self] in
+            self?.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
+    }
+    
+    func hideSeparator() {
+        DispatchQueue.main.async { [weak self] in
+            self?.separatorInset = UIEdgeInsets(top: 0, left: UIScreen.main.bounds.size.width, bottom: 0, right: 0)
+        }
+    }
+}
+
+
+extension UINavigationController {
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
