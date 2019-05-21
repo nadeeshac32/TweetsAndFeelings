103ef79de91722bb4985f0b2b22379fa26c56991
Nadeesha Lakmal <nadeeshal@codegen.net>
2019-05-21 21:23:30 +0530

    * Refactoring Sentiment getFeeling method

--- Tweets and feelings/Tweets and feelings/Models/GoogleSentiment/Sentiment.swift
+++ Tweets and feelings/Tweets and feelings/Models/GoogleSentiment/Sentiment.swift
@@ -9,32 +9,39 @@
 import ObjectMapper
 
 class Sentiment: NSObject, Mappable {
-	var magnitude     	: Float? = 0.0
-	var score 			: Float? = 0.0
+	var magnitude     	        : Float? = 0.0
+	var score 			        : Float? = 0.0
 	
 	required init?(map: Map) { }
 	
 	func mapping(map: Map) {
-		magnitude		<- map["magnitude"]
-		score	 		<- map["score"]
+		magnitude		        <- map["magnitude"]
+		score	 		        <- map["score"]
 	}
 	
 	func getFeeling() -> String {
-		var feeling: String = ""
-		if let magnitude = magnitude, magnitude > 0.15 {
-			feeling 	+= "Clearly "
-		}
-		
+		var feeling             : String = ""
 		if let score = score {
 			if score > 0.25 {
-				feeling 	+= "Positive"
-			} else if score > -0.25 {
-				feeling 	+= "Neutral"
+                if let magnitude = magnitude, magnitude > 0.4 {
+                    feeling     += "Clearly Positive"
+                } else {
+                    feeling     += "Positive"
+                }
+			} else if score < -0.25 {
+                if let magnitude = magnitude, magnitude > 0.4 {
+                    feeling     += "Clearly Negative"
+                } else {
+                    feeling     += "Negative"
+                }
 			} else {
-				feeling 	+= "Negative"
+                if let magnitude = magnitude, magnitude > 0.4 {
+                    feeling     += "Neutral"
+                } else {
+                    feeling     += "Mixed"
+                }
 			}
 		}
-		
 		return feeling
 	}
 }
